84e0cb29d6a4ee8d78e504dc3af1054a921a0dea
35191 <sofia_araujo_gdl@hotmail.com>
2025-01-30 16:53:34 +0000

    Initial migration

---
+++ StorageAutomotiveProducts/Data/BusinessDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StorageAutomotiveProducts.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageAutomotiveProducts.Data
+{
+    public class BusinessDbContext : DbContext
+    {
+        DbSet<Products> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=SQL6033.site4now.net;Initial Catalog=db_ab23ce_davidaleluia;User Id=db_ab23ce_davidaleluia_admin;Password=Passcode007");
+        }
+    }
+}
+
